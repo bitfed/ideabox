dbe93ca81f85f8d8dfdba1bbcb717f35237c412f
bitfed <bitfed@live.com>
2015-06-04 15:52:23 -0700

    made linkPreview into component

--- public/components/ideabox.jsx
+++ public/components/ideabox.jsx
@@ -3,53 +3,76 @@ var ideaDB = [
     title: "This Is A Title",
     description: "Aliquip eruditionem a proident. Ne dolor imitarentur. E velit voluptatibus o commodo anim multos tempor amet, possumus anim noster aliquip quem, culpa commodo o exquisitaque, laborum et offendit quo ne ipsum deserunt domesticarum, eu sint doctrina aliquip nam dolore iudicem ubi quibusdam. Incurreret nisi iis expetendis sempiternum ea vidisse quorum pariatur. Sint ut ab veniam nescius, an mentitum praetermissum. Ne anim illustriora, sint occaecat aut possumus, nam aliquip eruditionem et de in illustriora ex eu qui elit probant hic iis senserit aut eiusmod sed labore eu laborum id labore ut quem tempor an ullamco. Fore expetendis de voluptatibus e commodo duis minim se legam in quid comprehenderit occaecat labore probant, de mandaremus adipisicing. Deserunt si quae ad laboris lorem quo incididunt philosophari.",
     url: "http://yahoo.com/",
-    urlPreviewImage: "http://lorempixel.com/1000/600/animals",
-    urlPreviewTitle: "Things Run Around All Batshit",
-    urlPreviewDescription: "Aute expetendis hic probant, ubi enim appellat doctrina o vidisse despicationes ad excepteur aut nulla domesticarum nescius illum probant. E noster amet amet fabulas, excepteur ita litteris, ex quid non quem, an quamquam qui mentitum ex ab quis quis ipsum quamquam iis aut veniam in quis, fabulas eu consequat o id velit ut varias. Dolor ita mentitum ut multos. Esse id qui sunt mandaremus. Senserit hic duis. Mandaremus ea fore sed ab qui enim minim culpa, possumus duis tempor, hic legam quorum sed quamquam, in duis possumus nostrud, deserunt est quis litteris, tempor ab probant, varias ut eiusmod sed aliqua. Si e reprehenderit ut et magna non esse in esse o nostrud ad senserit exercitation te commodo."
+    urlPreview: {
+      image: "http://lorempixel.com/1000/600/animals",
+      title: "lorem sunt consectetur efflorescere",
+      description: "Possumus dolor qui officia sempiternum aut esse ubi mandaremus de noster, nescius anim fore litteris nulla, sed hic consectetur. Ita an quid quibusdam. Lorem vidisse et adipisicing, ingeniis si senserit et nulla a vidisse aut eiusmod amet appellat deserunt ea fugiat aliquip praesentibus, in hic quorum deserunt hic nam quorum excepteur distinguantur si iis nam legam vidisse. Mandaremus fore vidisse, illum ita ea illum iudicem ea si irure cohaerescant aut ad eu eram tamen nulla. Si pariatur efflorescere, sunt quo admodum o enim admodum eu fore quis ea nescius labore eu quamquam domesticarum. Fugiat quamquam de domesticarum. Se cernantur instituendarum, tamen est possumus, eiusmod fidelissimae qui nescius id te fore deserunt arbitrantur."
+    }
   },
   {
     title: "cohaerescant instituendarum illum praetermissum iis",
     description: "Proident eram et laboris exquisitaque. Dolore probant distinguantur, quamquam amet quibusdam singulis ea id lorem enim duis aliquip ea nam esse laboris praesentibus. Nostrud eram nescius, quibusdam iis proident, iis aut minim excepteur sed mentitum veniam do appellat tractavissent. Cillum proident domesticarum, deserunt magna quis si nisi, si aliqua quamquam voluptate ubi sed varias eiusmod singulis e enim id ab amet litteris ut labore comprehenderit senserit esse mentitum ne appellat ea varias consequat, do lorem hic fugiat. Ea varias nostrud eruditionem. Irure de doctrina si malis. Summis a quamquam eu noster e quis aut ullamco sed duis. Nescius se lorem ita lorem occaecat te occaecat, fabulas ne velit, commodo illustriora aut possumus an de quem proident, quibusdam illum offendit, offendit varias sed senserit familiaritatem, ne expetendis se iudicem.",
     url: "http://youtube.com",
-    urlPreviewImage: "http://lorempixel.com/1000/600/animals",
-    urlPreviewTitle: "iis e dolore de",
-    urlPreviewDescription: "Enim firmissimum commodonisi probant, lorem arbitror se firmissimum. Ex consequat de cupidatat. Sed nulla probant tractavissent. Iis dolore laborum, aute singulis an laborum, possumus sunt ubi admodum adipisicing an ea quo exquisitaque. Vidisse a anim, fore vidisse non magna legam ita culpa mentitum qui sempiternum sed multos fabulas cohaerescant ab non multos in anim, ne e quid varias cillum, dolor singulis comprehenderit an eiusmod lorem labore appellat lorem. Dolore fabulas litteris, et singulis an fabulas, sed duis ea dolore est legam de possumus. Sunt qui iudicem nam expetendis illustriora de mandaremus."
+    urlPreview: {
+      image: "http://lorempixel.com/1000/600/animals",
+      title: "officia officia irure incididunt",
+      description: "Minim id est nisi occaecat. Litteris quis ex ingeniis adipisicing. Ipsum e offendit an id in exercitation in malis exquisitaque cupidatat multos senserit, de senserit imitarentur id eiusmod fidelissimae de fabulas ut quem mentitum do tractavissent, deserunt ab velit, id ubi malis elit lorem. A tamen mentitum voluptatibus, est incididunt de excepteur, ipsum consectetur probant quid officia, ne quid expetendis, quis quibusdam firmissimum ubi iudicem magna tempor, ea nisi exercitation qui ad multos nescius. An labore anim sunt laborum. Noster officia firmissimum ubi in ut sunt sint veniam. An anim distinguantur te offendit noster irure proident noster, id ea varias probant in e veniam ipsum aliqua cupidatat. Noster expetendis an imitarentur eu id ipsum fidelissimae."
+    }
   },
   {
   title: "appellat noster arbitrantur ex esse",
   description: "Eiusmod est pariatur, laborum ex iudicem ubi eu aute non aute, laborum te fore de noster occaecat exercitation, incididunt ita irure sed an an comprehenderit, an magna nescius. Ne enim commodo cohaerescant. Consequat noster incididunt laborum, hic lorem quem enim mentitum. Litteris sunt admodum nostrud, nostrud aute laboris, enim ullamco do mentitum, quis qui mandaremus ne sunt. Est culpa eiusmod, sint iudicem si possumus, arbitror ex amet et senserit efflorescere id voluptate de consequat exquisitaque eu cupidatat, est velit ea labore et et se firmissimum, eu multos quamquam illustriora. Se quem cohaerescant se expetendis hic elit sed quibusdam enim vidisse a possumus magna vidisse fabulas iis tamen eu pariatur sed minim, ubi multos noster non ingeniis ex laborum ipsum pariatur, cupidatat qui mentitum. Minim aut arbitror, ingeniis illum quamquam an tamen consequat concursionibus o nam irure incurreret ullamco, singulis ab esse iudicem, enim an et summis proident, pariatur cillum laboris, culpa aliquip in consequat.",
     url: "http://youtube.com",
-    urlPreviewImage: "http://lorempixel.com/1000/600/animals",
-    urlPreviewTitle: "senserit aute distinguantur proident",
-    urlPreviewDescription: "Nam appellat e quamquam. Cillum eiusmod ita sempiternum. Culpa offendit ex illustriora iis magna ad non fugiat eiusmod a de iis fidelissimae aut tempor ad quis deserunt, irure ullamco ea quibusdam si cillum incurreret arbitrantur, quae ullamco aliquip, vidisse aute quamquam laboris. Eu sint noster ex eiusmod te aliqua expetendis expetendis, nam labore efflorescere ne ab cillum te sint. Sed veniam dolor iis pariatur, in fugiat offendit. Quid an fabulas de ipsum, de enim aliquip, in nulla veniam non possumus ex a ingeniis imitarentur. Singulis noster dolor incididunt dolore."
+    urlPreview: {
+      image: "http://lorempixel.com/1000/600/animals",
+      title: "ex arbitror laborum velit",
+      description: "Ad nulla quibusdam. Deserunt lorem doctrina quamquam, mandaremus culpa lorem et culpa. O te praetermissum, velit se vidisse ne velit iis qui ex multos officia, fugiat praetermissum deserunt aliqua mandaremus qui tempor de quorum mandaremus et commodo ubi doctrina ad nostrud hic quae hic offendit si culpa mandaremus. A o eram summis elit, dolore ea excepteur aut quis. Ad fugiat incurreret quibusdam. Quae exquisitaque deserunt esse laborum. Iis cillum irure aute ullamco o ex labore tractavissent, dolore ea deserunt, e culpa philosophari."
+    }
   },
   {
   title: "magna aliqua tempor pariatur esse",
   description: "Deserunt fugiat te pariatur distinguantur. Voluptate ad nescius. De illum tempor cupidatat, ingeniis quid illum quo minim. Eiusmod quorum legam ita illum. Ne summis magna hic voluptate an ubi si magna cillum illum, mandaremus magna legam nam cillum ubi admodum o velit officia id si quem coniunctione an pariatur duis nam ullamco distinguantur eu incurreret ad varias, appellat noster elit quo anim. Eram cernantur an lorem nisi, labore iudicem de reprehenderit ubi incurreret minim appellat incididunt, magna ullamco do reprehenderit. Illum despicationes expetendis legam fabulas.",
     url: "http://youtube.com",
-    urlPreviewImage: "http://lorempixel.com/1000/600/animals",
-    urlPreviewTitle: "consectetur ingeniis incurreret despicationes",
-    urlPreviewDescription: "Arbitror varias nostrud ingeniis et irure ullamco o quis noster. Eiusmod e malis, enim appellat se ingeniis quo vidisse veniam velit a esse, deserunt efflorescere se iudicem non qui mentitum philosophari te labore sempiternum possumus eram appellat, enim non officia se quem ea mandaremus esse quis laborum sint. Arbitror graviterque quo arbitror. Enim quamquam familiaritatem, occaecat minim mentitum. Legam voluptatibus probant culpa ullamco, e minim quorum enim tempor ab quibusdam dolor summis ab malis ex in labore est quorum. Eram quamquam ab labore varias ex fugiat de eiusmod hic nisi, legam instituendarum nescius minim laboris nam cernantur nisi cillum de summis et amet mandaremus o officia, quamquam quae irure de aute an se dolore quae fugiat voluptate, sint expetendis ubi laboris. Nisi tempor non labore esse."
+    urlPreview: {
+      image: "http://lorempixel.com/1000/600/animals",
+      title: "tamen incididunt adipisicing irure",
+      description: "Non ea eruditionem ne cillum cohaerescant excepteur aliqua consequat id eu cernantur quo probant, esse nescius e consectetur ut admodum quis nulla fabulas aliqua, quorum si cernantur quo aliqua iudicem transferrem, veniam ullamco qui possumus. Arbitror esse offendit ex ab velit se nisi nam hic vidisse tractavissent, aliqua commodo o coniunctione, quis nostrud ne quid enim de noster et aut duis singulis quo non quorum adipisicing in et irure iis irure. Nostrud de legam nostrud, offendit ipsum consequat quibusdam. Admodum hic cillum arbitror ex te ut magna quae quorum, fore comprehenderit tempor tamen nescius, vidisse nisi deserunt incurreret. Incididunt est nulla. Ita de instituendarum e sed eu legam admodum sed laboris nisi proident aliquip, appellat non elit. Cillum nostrud hic arbitror, ad ab nulla nisi amet."
+    }
   },
   {
   title: "anim laborum coniunctione iis iudicem",
   description: "Ex quibusdam consectetur. Sint tempor de veniam illum sed hic incurreret ita deserunt ita ubi anim iis labore qui si arbitror ut senserit ubi nisi ad proident a quamquam ab velit singulis in incididunt multos dolor a summis et o multos et minim. Amet transferrem expetendis velit litteris a amet exercitation eiusmod anim quibusdam. Proident qui mandaremus ubi quorum singulis hic laboris ad qui quorum incurreret de sunt cupidatat cohaerescant. Incurreret nisi a admodum tractavissent, do proident exquisitaque. Laborum e enim.",
     url: "http://youtube.com",
-    urlPreviewImage: "http://lorempixel.com/1000/600/animals",
-    urlPreviewTitle: "philosophari esse nam et",
-    urlPreviewDescription: "Tamen hic o quae vidisse. Quo tamen mandaremus et aut quid dolore in probant, quibusdam aute do incurreret reprehenderit si hic duis si irure. Ea lorem ullamco pariatur se quem eu cernantur hic labore. Ullamco qui laboris, irure senserit vidisse. A sunt arbitrantur, id quorum nulla lorem consequat et ea veniam voluptate excepteur ne ubi cillum doctrina concursionibus, tempor anim minim iis enim, ingeniis enim de admodum graviterque, cernantur aliqua ea doctrina firmissimum, quibusdam culpa sint doctrina malis. Nescius familiaritatem ab expetendis. Ita sunt voluptate ad ab nisi transferrem. Incididunt summis senserit est aliquip sint eram nam illum."
+    urlPreview: {
+      image: "http://lorempixel.com/1000/600/animals",
+      title: "philosophari esse nam et",
+      description: "Tamen hic o quae vidisse. Quo tamen mandaremus et aut quid dolore in probant, quibusdam aute do incurreret reprehenderit si hic duis si irure. Ea lorem ullamco pariatur se quem eu cernantur hic labore. Ullamco qui laboris, irure senserit vidisse. A sunt arbitrantur, id quorum nulla lorem consequat et ea veniam voluptate excepteur ne ubi cillum doctrina concursionibus, tempor anim minim iis enim, ingeniis enim de admodum graviterque, cernantur aliqua ea doctrina firmissimum, quibusdam culpa sint doctrina malis. Nescius familiaritatem ab expetendis. Ita sunt voluptate ad ab nisi transferrem. Incididunt summis senserit est aliquip sint eram nam illum."
+    }
   }
 /*
   {
     title: "lorem_w5",
     description: "lorem_nowrap",
     url: "http://youtube.com",
-    urlPreviewImage: "http://lorempixel.com/1000/600/animals",
-    urlPreviewTitle: "lorem_w4",
-    urlPreviewDescription: "lorem_nowrap"
+    urlPreview: {
+      image: "http://lorempixel.com/1000/600/animals",
+      title: "lorem_w4",
+      description: "lorem_nowrap"
+    }
   }
 */
 ];
+var LinkPreview = React.createClass({
+  render: function () {
+    return (
+      <div className="linkPreview">
+        <img src="http://lorempixel.com/1000/600/animals" alt="link preview image" />
+        <h2 className="title">{this.props.title}</h2>
+        <article><p>{this.props.description}</p></article>
+      </div>
+    );
+  }
+});
 var Idea = React.createClass({
   render: function () {
     return(
@@ -60,18 +83,9 @@ var Idea = React.createClass({
             <div className="ideaLogo"></div>
             <h1 className="ideaTitle">{this.props.idea.title}</h1>
           </header>
-          <article className="ideaDescription"><p>Dolor ita aut fore
-            admodum, an amet eruditionem, ab aut dolor doctrina, ab cillum eu magna, ita
-            nisi sint ubi senserit,an amet eruditionem, ab aut dolor doctrina, ab cillum eu magna, ita
-            nisi sint ubi senserit, admodum a incididunt, fugiat mandaremus familiaritatem
-            iis ne singulis et cernantur.</p>
+          <article className="ideaDescription"><p>{this.props.idea.description}</p>
           </article>
-          <div className="linkPreview">
-            <img src="http://lorempixel.com/1000/600/animals" alt="link preview image" />
-            <h2 className="title">Things Run Around All Batshit</h2>
-            <p>Multos nostrud eu quibusdam. Aliquip enim nostrud. Minim proident eu velit quid,
-              offendit duis commodo proident.</p>
-          </div>
+          <LinkPreview urlPreview={this.props.idea.urlPreview} />
         </div>
         <div className="fade"></div>
         <button className="pure-button readMore">Read More</button>

0624f5b589887c9ca0c074a768fb260a577bd2f7
bitfed <bitfed@live.com>
2015-06-11 10:56:27 -0700

    updated to react components

--- public/components/ideabox.jsx
+++ public/components/ideabox.jsx
@@ -7,7 +7,8 @@ var ideaDB = [
       image: "http://lorempixel.com/1000/600/animals",
       title: "lorem sunt consectetur efflorescere",
       description: "Possumus dolor qui officia sempiternum aut esse ubi mandaremus de noster, nescius anim fore litteris nulla, sed hic consectetur. Ita an quid quibusdam. Lorem vidisse et adipisicing, ingeniis si senserit et nulla a vidisse aut eiusmod amet appellat deserunt ea fugiat aliquip praesentibus, in hic quorum deserunt hic nam quorum excepteur distinguantur si iis nam legam vidisse. Mandaremus fore vidisse, illum ita ea illum iudicem ea si irure cohaerescant aut ad eu eram tamen nulla. Si pariatur efflorescere, sunt quo admodum o enim admodum eu fore quis ea nescius labore eu quamquam domesticarum. Fugiat quamquam de domesticarum. Se cernantur instituendarum, tamen est possumus, eiusmod fidelissimae qui nescius id te fore deserunt arbitrantur."
-    }
+    },
+    id: "01"
   },
   {
     title: "cohaerescant instituendarum illum praetermissum iis",
@@ -17,7 +18,8 @@ var ideaDB = [
       image: "http://lorempixel.com/1000/600/animals",
       title: "officia officia irure incididunt",
       description: "Minim id est nisi occaecat. Litteris quis ex ingeniis adipisicing. Ipsum e offendit an id in exercitation in malis exquisitaque cupidatat multos senserit, de senserit imitarentur id eiusmod fidelissimae de fabulas ut quem mentitum do tractavissent, deserunt ab velit, id ubi malis elit lorem. A tamen mentitum voluptatibus, est incididunt de excepteur, ipsum consectetur probant quid officia, ne quid expetendis, quis quibusdam firmissimum ubi iudicem magna tempor, ea nisi exercitation qui ad multos nescius. An labore anim sunt laborum. Noster officia firmissimum ubi in ut sunt sint veniam. An anim distinguantur te offendit noster irure proident noster, id ea varias probant in e veniam ipsum aliqua cupidatat. Noster expetendis an imitarentur eu id ipsum fidelissimae."
-    }
+    },
+    id: "02"
   },
   {
   title: "appellat noster arbitrantur ex esse",
@@ -27,7 +29,8 @@ var ideaDB = [
       image: "http://lorempixel.com/1000/600/animals",
       title: "ex arbitror laborum velit",
       description: "Ad nulla quibusdam. Deserunt lorem doctrina quamquam, mandaremus culpa lorem et culpa. O te praetermissum, velit se vidisse ne velit iis qui ex multos officia, fugiat praetermissum deserunt aliqua mandaremus qui tempor de quorum mandaremus et commodo ubi doctrina ad nostrud hic quae hic offendit si culpa mandaremus. A o eram summis elit, dolore ea excepteur aut quis. Ad fugiat incurreret quibusdam. Quae exquisitaque deserunt esse laborum. Iis cillum irure aute ullamco o ex labore tractavissent, dolore ea deserunt, e culpa philosophari."
-    }
+    },
+    id: "03"
   },
   {
   title: "magna aliqua tempor pariatur esse",
@@ -37,7 +40,8 @@ var ideaDB = [
       image: "http://lorempixel.com/1000/600/animals",
       title: "tamen incididunt adipisicing irure",
       description: "Non ea eruditionem ne cillum cohaerescant excepteur aliqua consequat id eu cernantur quo probant, esse nescius e consectetur ut admodum quis nulla fabulas aliqua, quorum si cernantur quo aliqua iudicem transferrem, veniam ullamco qui possumus. Arbitror esse offendit ex ab velit se nisi nam hic vidisse tractavissent, aliqua commodo o coniunctione, quis nostrud ne quid enim de noster et aut duis singulis quo non quorum adipisicing in et irure iis irure. Nostrud de legam nostrud, offendit ipsum consequat quibusdam. Admodum hic cillum arbitror ex te ut magna quae quorum, fore comprehenderit tempor tamen nescius, vidisse nisi deserunt incurreret. Incididunt est nulla. Ita de instituendarum e sed eu legam admodum sed laboris nisi proident aliquip, appellat non elit. Cillum nostrud hic arbitror, ad ab nulla nisi amet."
-    }
+    },
+    id: "04"
   },
   {
   title: "anim laborum coniunctione iis iudicem",
@@ -47,7 +51,8 @@ var ideaDB = [
       image: "http://lorempixel.com/1000/600/animals",
       title: "philosophari esse nam et",
       description: "Tamen hic o quae vidisse. Quo tamen mandaremus et aut quid dolore in probant, quibusdam aute do incurreret reprehenderit si hic duis si irure. Ea lorem ullamco pariatur se quem eu cernantur hic labore. Ullamco qui laboris, irure senserit vidisse. A sunt arbitrantur, id quorum nulla lorem consequat et ea veniam voluptate excepteur ne ubi cillum doctrina concursionibus, tempor anim minim iis enim, ingeniis enim de admodum graviterque, cernantur aliqua ea doctrina firmissimum, quibusdam culpa sint doctrina malis. Nescius familiaritatem ab expetendis. Ita sunt voluptate ad ab nisi transferrem. Incididunt summis senserit est aliquip sint eram nam illum."
-    }
+    },
+    id: "05"
   }
 /*
   {
@@ -67,17 +72,25 @@ var LinkPreview = React.createClass({
     return (
       <div className="linkPreview">
         <img src="http://lorempixel.com/1000/600/animals" alt="link preview image" />
-        <h2 className="title">{this.props.title}</h2>
-        <article><p>{this.props.description}</p></article>
+        <h2 className="title">{this.props.urlPreview.title}</h2>
+        <article><p>{this.props.urlPreview.description}</p></article>
       </div>
     );
   }
 });
 var Idea = React.createClass({
+  getInitialState: function () {
+/*    console.log(this);*/
+    return {cardState: false};
+  },
+  handleClick: function() {
+    console.log('button of ' + this + ' was clicked.');
+    this.setState({cardState: !this.state.cardState});
+  },
   render: function () {
     return(
       <div className="idea">
-        <div className="ideaCard">
+      <div className={this.state.cardState ? 'ideaCard-expanded' : 'ideaCard'}>
           <header className="ideaHeader">
             <div className="voter"></div>
             <div className="ideaLogo"></div>
@@ -88,7 +101,7 @@ var Idea = React.createClass({
           <LinkPreview urlPreview={this.props.idea.urlPreview} />
         </div>
         <div className="fade"></div>
-        <button className="pure-button readMore">Read More</button>
+        <button className="pure-button readMore" onClick={this.handleClick}>Read More</button>
       </div>
     );
   }
@@ -97,11 +110,13 @@ var Idea = React.createClass({
 var Ideabox = React.createClass({
   render: function () {
     var ideaBox = [];
-    this.props.ideaList.forEach(function (idea) {
-      ideaBox.push(<Idea idea={idea} />);
+    this.props.ideaList.map(function (idea) {
+    ideaBox.push(<Idea key={idea.id} idea={idea} />);
     });
     return (
-      <div className="ideaBox">{ideaBox}</div>
+      <div className="ideaBox">
+        {ideaBox}
+      </div>
     );
   }
 });
